001cd7a5fb9946a9e6e1ecb8ebcd89d6ac5aacbe
Samantha Padilla <samantha.227478@nv.ccsd.net>
2020-09-06 05:31:16 +0000

    added vars for the hour hand, hours, degrees passed, minuets passed, and the combination of minuets passed, hour hand, and degrees.

--- src/main/java/AnalogClock.java
+++ src/main/java/AnalogClock.java
@@ -7,12 +7,18 @@ class AnalogClock {
         Scanner scanner = new Scanner(System.in);
 
         //Reads ints from user
-
+        int hHand = scanner.nextInt();
 
         /*
          *  your code goes here
          */
 
+        int hours = hHand / 30;
+        int dPast = hours * 30;
+        int mPast = hHand - dPast;
+        int mHandDg = (30 - mPast) * 6;
+
+        System.out.print(mHandDg);
         // closing the scanner object
         scanner.close();
     }
